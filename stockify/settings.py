cb4587a68466fdda9215357e68f7a71c89fe8a8f
Cr1stianNb <cristiannettle1@gmail.com>
2025-09-12 22:04:21 -0300

    add py_cache__ to gitignore

--- stockify/settings.py
+++ stockify/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'product.apps.ProductConfig'
+    'product.apps.ProductConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
