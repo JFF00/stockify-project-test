756b13e349942678f64d54ebca7cf95beff2d300
Diego Martinez <diego.martinez01@alumnos.ucn.cl>
2025-09-28 19:26:34 -0300

    implementacion completa de los serializers

--- Backend/README.md
+++ Backend/README.md
@@ -1,12 +1,20 @@
 # Comandos para levantar el contenedor para desarrollo
+
 - docker-compose build
 - docker-compose up
 - docker-compose run web python manage.py migrate
 
 # Crear super usuario
+
 - docker-compose run web python manage.py migrate
 
 # Actualmente se cuenta con hot reloading pero ojo
+
 - .El autoreload de Django no siempre detecta cambios si agregas nuevos archivos o cambias configuraciones muy internas → en esos casos hay que reiniciar el contenedor.
 
 . En modo producción no se usa runserver, se usa gunicorn o uvicorn → y ahí normalmente no hay hot reload.
+
+# Proceso para realizar las migraciones
+
+- docker exec -it stockify-django python manage.py migrate
+- docker exec -it stockify-django python manage.py makemigrations
--- Backend/stock/views.py
+++ Backend/stock/views.py
@@ -1,13 +1,21 @@
 from rest_framework import viewsets 
-from .models import Category, Product
-from .serializers import CategorySerializer, ProductSerializer
+from .models import Category, Product, Movement, Record
+from .serializers import CategorySerializer, ProductSerializer, MovementSerializer, RecordSerializer
 
 # Create your views here.
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
+    
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class MovementViewSet(viewsets.ModelViewSet):
+    queryset = Movement.objects.all()
+    serializer_class = MovementSerializer
+
+class RecordViewSet(viewsets.ModelViewSet):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
