b432c8164644cc98b8f96cffaaf241e1a45c8d8a
JFF00 <juan.maltes@alumnos.ucn.cl>
2025-10-07 01:07:10 -0300

    mm

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -6,8 +6,8 @@ class User(AbstractUser):
     role = models.CharField(max_length=50, default='user')
     #user_id = models.AutoField(primary_key=True)
     ##username = models.TextField()
-    email = models.EmailField()
+    #email = models.EmailField()
     #role = models.TextField() # change later
-    password = models.TextField() #change later
+    #password = models.TextField() #change later
 
 
